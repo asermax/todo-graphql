7e13744e7d36c5c9b4a3e64f5c00a3e53fc521be
Agustin Carrasco <asermax@gmail.com>
2017-10-17 00:44:01 -0300

    web: wire toggle and styles for the todo items

--- web/src/pages/List/index.jsx
+++ web/src/pages/List/index.jsx
@@ -3,19 +3,24 @@ import classNames from 'classnames'
 import { connect } from 'react-redux'
 import { compose, flattenProp, setDisplayName } from 'recompose'
 import { getCurrentList } from 'data/list/selectors'
+import { toggleItem } from 'data/item/actions'
 import styles from './styles.scss'
 
 const mapStateToProps = (state) => ({
   list: getCurrentList(state),
 })
 
+const mapDispatchToProps = (dispatch) => ({
+  toggleItem: (listId, id) => dispatch(toggleItem(listId, id)),
+})
+
 const enhancer = compose(
-  connect(mapStateToProps),
+  connect(mapStateToProps, mapDispatchToProps),
   flattenProp('list'),
   setDisplayName('List'),
 )
 
-const List = enhancer(({ name, items }) => (
+const List = enhancer(({ _id, name, items, toggleItem }) => (
   <div>
     <div className="card-body">
       <h4 className="card-title">
@@ -24,10 +29,17 @@ const List = enhancer(({ name, items }) => (
     </div>
     <ul className="list-group list-group-flush">
       {items.map((item) => (
-        <li key={item._id} className="list-group-item">
-          <div className={classNames('form-check', styles.customFormCheck)}>
+        <li key={item._id} className={classNames('list-group-item', styles.item, {
+            [styles.done]: item.done,
+          })}>
+          <div className="form-check">
             <label className="form-check-label">
-              <input className="form-check-input" type="checkbox"/>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                checked={item.done}
+                onChange={() => toggleItem(_id, item._id)}
+              />
               {item.text}
             </label>
           </div>
